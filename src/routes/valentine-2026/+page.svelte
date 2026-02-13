0d9abe7c6ead5c99585afb2aa05dc222c1ee2bba
Andira Rahman <andira.rahman@pertamc.com>
2026-02-13 09:46:30 +0700

    feat: 2026 Entry
    A simple love letter with a sneaky hidden message.

---
+++ src/routes/valentine-2026/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    // import type { PageData } from './$types';
+
+    // export let data: PageData;
+</script>
+
+<div class="flex justify-center">
+    <article class="max-w-100ch whitespace-pre-line">
+        <p class=" text-right">Friday, 13th February 2026</p>
+        Dearest, Maya
+        
+        Two years have passed since I last wrote to you here.
+        The last time, I gave you a faux choice as a form of jest.
+        This year, I have no desire to do so.
+
+        I longingly await your arrival in my embrace so that I may feel your warmth.
+        I yearn for the opportunity to gaze upon your beauty with my own eyes.
+        I have a question that have kept me from rest.
+        You must make haste and relieve me from my burden.
+        
+        Tell me sincerely my love
+        Will you be my valentine?
+
+        Write back to me so that I may sleep and dream of your love once more.
+
+        Lovingly yours,
+        Aam
+
+        <p class="text-black/10">
+            <span class="text-black/20">P.S.</span>
+            We will see each other soon
+            Do. Not. Bathe.
+        </p>
+    </article>
+</div>
