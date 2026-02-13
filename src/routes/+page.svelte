9917de000398da815144a662343cfd71a410b2e5
Andira Rahman <andira.rahman@pertamc.com>
2026-02-13 10:07:43 +0700

    feat: Add simple directory as homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,8 @@
     // export let data: PageData;
 </script>
 
-<h1 class="text-4xl font-bold">Sorry!</h1>
-<p>This page is still a work in progress...</p>
+<h1 class="text-4xl font-bold mb-4">Welcome, Maya</h1>
+<ul class="space-y-2">
+    <li><a class="hover:underline hover:underline-offset-4" href="/valentine-2026">Valentine's 2026</a></li>
+    <li><a class="hover:underline hover:underline-offset-4" href="/valentine-2024">Valentine's 2024</a></li>
+</ul>
